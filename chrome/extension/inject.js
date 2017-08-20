7a4793a867d011aaa5f3ae814f3a338f4aa02c51
Doug Sheffer <doug.sheffer@researchsquare.com>
2017-08-20 17:11:45 -0400

    Refactor inject code

--- chrome/extension/inject.js
+++ chrome/extension/inject.js
@@ -1,17 +1,15 @@
 import Bands from '../../app/constants/Bands';
 
-var context;
-var filters;
+const context = new AudioContext();
+const fanOut = context.createGain();
+let filters;
 
-var init = () => {
-    if (context === undefined) {
-        context = new AudioContext();
-    }
-
+// Create a new filter chain and attach it to the fan out.
+const setFilters = (power, gains) => {
     if (filters === undefined) {
         // Create the chain of filters.
-        filters = Bands.map((band) => {
-            var filter = context.createBiquadFilter();
+        filters = Bands.map((band, i) => {
+            const filter = context.createBiquadFilter();
             filter.type = band.type;
             filter.gain.value = 0.0;
             filter.frequency.value = band.freq;
@@ -21,79 +19,81 @@ var init = () => {
         });
 
         // Connect the chain of filters.
-        for (var i = 0; i < filters.length - 1; ++i) {
+        for (let i = 0; i < filters.length - 1; ++i) {
             filters[i].connect(filters[i + 1]);
         }
 
         // Connect the filter chain to the sink.
         filters[filters.length - 1].connect(context.destination);
     }
+
+    // Set new gain on each filter.
+    gains.forEach((gain, i) => {
+        filters[i].gain.value = gain;
+    });
+
+    fanOut.disconnect();
+    if (power) {
+        fanOut.connect(filters[0]);
+    } else {
+        fanOut.connect(context.destination);
+    }
 };
 
-var attach = () => {
-    init();
-
-    var i;
-    var sources = [];
+// Find sources and attach them to the fan out.
+const attachFilters = () => {
+    const sources = [];
 
     // Find audio tags.
-    var audioNodes = document.getElementsByTagName('audio');
-    for (i = 0; i < audioNodes.length; ++i) {
+    const audioNodes = document.getElementsByTagName('audio');
+    for (let i = 0; i < audioNodes.length; ++i) {
         sources.push(audioNodes[i]);
     }
 
     // Find video tags.
-    var videoNodes = document.getElementsByTagName('video');
-    for (i = 0; i < videoNodes.length; ++i) {
+    const videoNodes = document.getElementsByTagName('video');
+    for (let i = 0; i < videoNodes.length; ++i) {
         sources.push(videoNodes[i]);
     }
 
     // Connect each source to the filter chain.
     sources.forEach((el) => {
         try {
-            var source = context.createMediaElementSource(el);
-            source.connect(filters[0]);
-
-            console.log('EQ: Attached');
+            const source = context.createMediaElementSource(el);
+            source.connect(fanOut);
         } catch (e) {
         }
     });
 };
 
-window.addEventListener('load', () => {
-    attach();
+// Update the current filter state.
+const updateState = () => {
+    chrome.storage.local.get('state', (obj) => {
+        const state = JSON.parse(obj.state);
 
-    var observer = new MutationObserver(attach);
+        const power = state.equalizer.power;
+        const gains = state.equalizer.gains;
+
+        setFilters(power, gains);
+    });
+};
+
+window.addEventListener('load', () => {
+    // Attach filters and update state on page load.
+    attachFilters();
+    updateState();
+
+    // Attach filters on page mutation.
+    const observer = new MutationObserver(attachFilters);
     observer.observe(document.body ? document.body : document, {
         childList: true,
-        subtree: false,
-        attributes: false,
-        characterData: false
+        subtree: true,
     });
 
+    // Update state on update event.
     chrome.runtime.onMessage.addListener((message) => {
         if (message === 'update') {
-            chrome.storage.local.get('state', (obj) => {
-                const state = JSON.parse(obj.state);
-
-                const power = state.equalizer.power;
-                const gains = state.equalizer.gains;
-
-                // Reset gain on each filter.
-                filters.forEach((filter) => {
-                    filter.gain.value = 0.0;
-                });
-
-                // End here if the power is off.
-                if (!power) {
-                    return;
-                }
-
-                // Set new gain on each filter.
-                gains.forEach((gain, i) => {
-                    filters[i].gain.value = gain;
-                });
-            });
+            updateState();
         }
     });
 });
